6aa6c252f1bade633878ece7ca76086fa1de016d
Mari Borilla <mffborilla@donbosco.edu.ph>
2024-08-25 12:05:11 +0300

    Add initial content

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Dynamically load HTML content from separate files into a web page.
+    const header = document.getElementById('header');
+    const content = document.getElementById('content');
+    const footer = document.getElementById('footer');
+
+    fetch('includes/header.html')
+        .then(response => response.text())
+        .then(data => {
+            header.innerHTML = data;
+        });
+
+    fetch('includes/content.html')
+        .then(response => response.text())
+        .then(data => {
+            content.innerHTML = data;
+        });
+
+    fetch('includes/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            footer.innerHTML = data;
+        });
+
+});
